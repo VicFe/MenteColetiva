943f7dc54b8e10671855b93571aac790acc33bd8
VicFe <victorfernando99831143@gmail.com>
2025-12-05 10:03:50 -0300

    Aula de lógica da programação (Victor)

---
+++ victor/5. Lógica da Programação - JavaScript/script.js
@@ -0,0 +1,15 @@
+var nome = "Victor Fernando";
+var idade = 20;
+var carteira = 1.2;
+const cidade = "Trairi";
+
+console.log(
+  "Meu nome é ",
+  nome,
+  " tenho ",
+  idade,
+  " e sou bem rico com R$",
+  carteira,
+  " na minha carteira, e moro em ",
+  cidade
+);
